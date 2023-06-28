739d79241de9b82081976ae7141459b9e32bebaa
AllenJames <allenjames.laxamana@gmail.com>
2023-06-28 20:34:07 +0800

    Add database connection to "bankdb" MySQL database

--- src/main/java/com/mycompany/cs/grp8/sd1a/dbConnection.java
+++ src/main/java/com/mycompany/cs/grp8/sd1a/dbConnection.java
@@ -2,20 +2,39 @@ package com.mycompany.cs.grp8.sd1a;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.swing.JOptionPane;
 
-
-
 public class dbConnection {
-    
-    public static Connection con() {
+
+    private Connection con;
+    private static dbConnection instance;
+
+    dbConnection() {
         Connection con = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/shadifin tech","root","");
-        } catch(Exception e) {
-            JOptionPane.showMessageDialog(null, e);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankdb", "root", "");
+            System.out.println("Connection Established");
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Cannot access database");
         }
+    }
+
+    public static dbConnection getDatabaseConnection() {
+        if (instance == null) {
+            instance = new dbConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
         return con;
+    }
+
+    public static void closeConnection() {
+        instance = null;
     }
 }
